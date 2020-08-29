355c7fbbecf085222b52055228f3e72b40206dae
Ian Atkinson <iatkinson209@gmail.com>
2020-08-29 15:00:36 +0100

    Dynamic time

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
+def main():
+    return render_template('index.html')
+
+@app.route('/timer')
 def timer():
-    return '\n'.join(('<head>',datetime.now().time().strftime('%H:%M:%S'),'</head>'))
+    time_str=datetime.now().time().strftime('%H:%M:%S')
+    return jsonify({"time":time_str})
 
 app.run()
 
